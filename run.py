a3a81451c7653d8cf629e72dfb8dfbc13715431c
Yi-Lin Sung <r06942076@ntu.edu.tw>
2021-04-22 01:46:54 -0400

    add run.py

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import argparse
 import torch
-from unsup3d import setup_runtime, Trainer, Unsup3D
+from unsup3d import setup_runtime, Trainer, Unsup3D, Classification, Unsup3D_Classifier
 
 
 ## runtime arguments
@@ -9,11 +9,15 @@ parser.add_argument('--config', default=None, type=str, help='Specify a config f
 parser.add_argument('--gpu', default=None, type=int, help='Specify a GPU device')
 parser.add_argument('--num_workers', default=4, type=int, help='Specify the number of worker threads for data loaders')
 parser.add_argument('--seed', default=0, type=int, help='Specify a random seed')
+parser.add_argument('--model', default="Unsup3D", type=str)
+parser.add_argument('--rotated_angle', type=float)
+parser.add_argument('--jitter_scale', type=float)
 args = parser.parse_args()
 
 ## set up
+model = eval(args.model)
 cfgs = setup_runtime(args)
-trainer = Trainer(cfgs, Unsup3D)
+trainer = Trainer(cfgs, model)
 run_train = cfgs.get('run_train', False)
 run_test = cfgs.get('run_test', False)
 
--- unsup3d/__init__.py
+++ unsup3d/__init__.py
@@ -1,3 +1,3 @@
 from .utils import setup_runtime
 from .trainer import Trainer
-from .model import Unsup3D
+from .model import Unsup3D, Classification, Unsup3D_Classifier
